6dca7460668a8e7ea60dbd84ad5e7e963898444a
notdotjson <dotjson413@proton.me>
2026-06-29 21:16:40 -0500

    learning rust: hello world

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let vers = "0.1.0";
+    let hi = "Hello World!";
+    
+    println!("{hi} I'm Inaya's first (ok maybe second x3) Rust program!");
+    
+    println!("This script is version {vers}!");
+
+    println!("wow");
+}
